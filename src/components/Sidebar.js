424f023adfc47a977419eff72ec4fc5df4df6323
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-01-14 13:50:59 -0300

    Feature: sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Toolbar from '@mui/material/Toolbar';
+
+
+const drawerWidth = 240;
+
+
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Divider />
+                <List>
+                    {['Home', 'Add new', 'Leaderboard'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemText primary={text} sx={{ textAlign: "right" }} />
+                        </ListItem>
+                    ))}
+                    <Divider />
+                    <ListItem button key={'Sign Out'}>
+                        <ListItemText primary={'Sign Out'} sx={{ textAlign: "right" }} />
+                    </ListItem>
+                </List>
+                <Toolbar />
+
+
+            </Drawer>
+        )
+    }
+}
+
+export default Sidebar
