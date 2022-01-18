f8c3de96ee8cadc8224f5ed3dc88817c28bfea5d
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-01-18 18:25:04 -0300

    Fix: There is no question

--- src/pages/AddNew.js
+++ src/pages/AddNew.js
@@ -2,59 +2,47 @@ import React, { Component, createRef } from 'react'
 import { Form, Button } from 'react-bootstrap';
 import Layout from '../components/Layout'
 
-const maxChars = { question: 400, answer: 255 }
-const minChars = { question: 400, answer: 255 }
-const charLimitWarn = 100
+const maxChars = 255
+const minChars = 10
+const charLimitWarn = 150
 
 class AddNew extends Component {
     textAreaRef = createRef()
-    state = { questionCount: 0, answer1Count: 0, answer2Count: 0 }
+    state = { answer1Count: 0, answer2Count: 0 }
 
     render() {
-        const { questionCount, answer1Count, answer2Count } = this.state
+        const { answer1Count, answer2Count } = this.state
         return (
             <Layout>
-                <h1>Create a new <i>'Would You Rather'</i></h1>
+                <h1>Create new</h1>
                 <br />
                 <Form>
-                    <Form.Group className="mb-3" controlId="questionId">
+                    <Form.Group className="mb-3" controlId="firstAnswer">
+                        <Form.Label><b>Would You Rather...</b></Form.Label>
                         <Form.Control
                             as="textarea"
-                            rows={3}
-                            ref={this.textInput}
-                            type="text"
-                            placeholder="Would you rather..."
-                            maxLength={maxChars.question}
-                            onChange={(e) => this.setState({ questionCount: e.target.value.length })}
-                        />
-                        <Form.Text className="text-muted">
-                            <b style={{ color: maxChars.question - questionCount < charLimitWarn ? "red" : "" }}>
-                                {maxChars.question - questionCount}
-                            </b> charecters remaining.
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="firstAnswer">
-                        <Form.Control
                             placeholder="Type the first answer"
-                            maxLength={maxChars.answer}
+                            rows={2}
+                            maxLength={maxChars}
                             onChange={(e) => this.setState({ answer1Count: e.target.value.length })}
                         />
                         <Form.Text className="text-muted">
-                            <b style={{ color: maxChars.answer - answer1Count < charLimitWarn ? "red" : "" }}>
-                                {maxChars.answer - answer1Count}
+                            <b style={{ color: maxChars - answer1Count < charLimitWarn ? "red" : "" }}>
+                                {maxChars - answer1Count}
                             </b> charecters remaining.
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="secondAnswer">
                         <Form.Control
+                            as="textarea"
                             placeholder="Type the second answer"
-                            maxLength={maxChars.answer}
+                            rows={2}
+                            maxLength={maxChars}
                             onChange={(e) => this.setState({ answer2Count: e.target.value.length })}
                         />
                         <Form.Text className="text-muted">
-                            <b style={{ color: maxChars.answer - answer2Count < charLimitWarn ? "red" : "" }}>
-                                {maxChars.answer - answer2Count}
+                            <b style={{ color: maxChars - answer2Count < charLimitWarn ? "red" : "" }}>
+                                {maxChars - answer2Count}
                             </b> charecters remaining.
                         </Form.Text>
                     </Form.Group>
