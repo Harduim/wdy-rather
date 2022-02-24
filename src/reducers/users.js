d159ca88df873068c7428624c01c5d2fdcfccda2
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-02-23 22:54:45 -0300

    Fix: add answer to user slice

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,8 +1,22 @@
 import { RECEIVE_DATA } from '../actions/shared'
+import { ANSWER_POOL } from '../actions/pools'
 
 export default function users(state = [], action) {
-    if (action.type === RECEIVE_DATA) {
-        return action.users
+    switch (action.type) {
+        case RECEIVE_DATA:
+            return action.users
+
+        case ANSWER_POOL:
+            const { poolId, option, userId } = action
+            return {
+                ...state,
+                [userId]: {
+                    ...state[userId],
+                    answers: { ...state[userId].answers, [poolId]: option }
+                }
+            }
+
+        default:
+            return state
     }
-    return state
 }
