d2fa2b27b10de4f05cdfa8b16617d2d42e4a44c0
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-03-01 21:22:31 -0300

    Feat: proto leaderboard

--- src/pages/Leaderboards.js
+++ src/pages/Leaderboards.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react'
 import Layout from '../components/Layout'
+import { connect } from 'react-redux'
+import Avatar from '../components/Avatar'
+import { Table } from 'react-bootstrap'
 
-const maxChars = 255
-const minChars = 10
-const charLimitWarn = 150
 
 class Leaderboard extends Component {
-  render () {
+  render() {
+    const { authedUser, pools, users } = this.props
+    const userIds = Object.keys(users).sort((a, b) => users[b].points - users[a].points)
+
+    userIds.forEach(
+      e => users[e]['points'] = Object.keys(users[e].answers).length + users[e].questions.length
+    )
+
     return (
       <Layout>
         <h1>Leaderboard</h1>
-        <br />
+        <Table className='leaderboard-table'>
+          <thead>
+            <tr>
+              <th>User</th>
+              <th>Questions</th>
+              <th>Answers</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              userIds.map(
+                i => {
+                  const u = users[i]
+                  return (
+                    <tr key={i}>
+                      <td>
+                        <Avatar user={u} />
+                      </td>
+                      <td>{u.points}</td>
+                      <td>{u.points}</td>
+                      <td>{u.points}</td>
+                    </tr>
+                  )
+                }
+              )
+            }
+          </tbody>
+        </Table>
       </Layout>
     )
   }
 }
 
-export default Leaderboard
+
+
+const mapStateToProps = state => {
+  return {
+    pools: state.pools,
+    authedUser: state.authedUser,
+    users: state.users
+  }
+}
+
+export default connect(mapStateToProps)(Leaderboard)
